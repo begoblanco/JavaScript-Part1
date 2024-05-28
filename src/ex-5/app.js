1e09603bd4c2ff49f952a0154b351c47e2370a76
Bego Blanco <begoblanfer@gmail.com>
2024-05-28 21:31:33 +0200

    ex5 and ex6

--- src/ex-5/app.js
+++ src/ex-5/app.js
@@ -1,26 +1,28 @@
 //Escribe un programa que pida dos números
 //y escriba en la pantalla cual es el mayor.
 
-const num1 = document.getElementById("number-1")
-const num2 = document.getElementById("number-2")
+const num1 = document.getElementById("number-1");
+const num2 = document.getElementById("number-2");
 const btnGreater = document.getElementById("btn-greater");
 const mainBlock = document.querySelector("main");
 
-function render(num1, num2, res){
-    mainBlock.innerHTML = `Entre ${num1} y ${num2} el mayor es: ${res}`;
+function render(msg) {
+  mainBlock.innerHTML = msg;
 }
 
-function greater(x,y){
-    if (x > y){
-        return render(x, y, x)
-    }
-    return render(x, y, y)
+function greater(x, y) {
+  if (x > y) {
+    return `Entre ${x} y ${y} el mayor es: ${x}`;
+  } 
+  if (x < y) {
+    return `Entre ${x} y ${y} el mayor es: ${y}`;
+  }
+  return `Son iguales`;
 }
 
 btnGreater.addEventListener("click", () => {
-    let n1 = num1.value;
-    let n2 = num2.value;
-  
-    greater(parseInt(n1), parseInt(n2));
-    
-  });
+  let n1 = num1.value;
+  let n2 = num2.value;
+
+  render(greater(parseInt(n1), parseInt(n2)));
+});
